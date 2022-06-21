3113eea703d6fd022b55e2267ef9a28b62db72b5
Pragesh-Yadav <pragesh.scert@gmail.com>
2022-06-21 15:29:57 +0530

    Https_status

--- src/Controller/authorController.js
+++ src/Controller/authorController.js
@@ -3,7 +3,7 @@ const authorModel = require("../Models/authorModel")
 const createAuthor = async function(req, res) {
     let autherData = req.body
     let saveAuthor = await authorModel.create(autherData)
-    res.send({status: true, data: saveAuthor})
+    res.status(201).send({status: true, data: saveAuthor})
 }
 
 module.exports.createAuthor = createAuthor
